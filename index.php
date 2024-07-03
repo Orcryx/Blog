afa1511815c2c358107f64a47182d367fbddc40d
Orcryx <lauryannmagar@gmail.com>
2024-07-03 12:33:35 +0200

    Add dotebv au projet et modifier router et databaseService pour l'utilisation de variable d'environnement

--- index.php
+++ index.php
@@ -3,9 +3,14 @@
 require "vendor/autoload.php";
 
 use App\service\RouterService;
+use Symfony\Component\Dotenv\Dotenv;
 
 //Init Router
 $router = new RouterService();
+
+//Init variables d'env
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/.env');
 
 //afficher le retour du routeur
 echo $router->run($_SERVER['REQUEST_URI']);
