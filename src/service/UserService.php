8d625e6c7bed8edbd517d7a3272d8d0a58d0d54f
Orcryx <lauryannmagar@gmail.com>
2024-07-10 12:03:25 +0200

    Modifier l'appel de la class pour ne pas avoir de paramètre (test)

--- src/service/UserService.php
+++ src/service/UserService.php
@@ -9,14 +9,14 @@ class UserService{
 
     private UserRepository $user;
 
-    public function __construct(UserRepository $user)
+    public function __construct()
     {
-        $this->user= $user;
+        $this->user= new UserRepository(new DatabaseService);
     }
 
     public function logIn($environnement): void
     {
-        $this->isConnected();
+        //$this->isConnected();
         $email = $_POST['email'];
         /** @var array|null  $user  **/
         $user = $this->user->getUserByEmail($email); 
@@ -24,14 +24,14 @@ class UserService{
             $hashedPassword = password_hash($user[0]["password"], PASSWORD_DEFAULT);
             if(password_verify($_POST['password'],  $hashedPassword))
             {
-                // echo "reussite !";
+                echo "reussite !";
                 $_SESSION['status'] = true;
                 $_SESSION['userId'] =$user[0]["userId"];
                 $_SESSION['email'] =$user[0]["email"];
                 $_SESSION['role'] = $user[0]["role"];
                 $_SESSION['nickname'] = $user[0]["nickname"];
                 $_SESSION['environnement'] = $environnement;
-                header("Location:".$_SESSION['environnement']);
+                //header("Location:".$_SESSION['environnement']);
        
             }
             else
@@ -45,12 +45,12 @@ class UserService{
      
     }
 
-    private function isConnected() : void
-    {
-        if (isset($_SESSION['status']) && $_SESSION['status'] === true) {
-            header("Location:".$_SESSION['environnement']);
-        }
-    }
+    // private function isConnected() : void
+    // {
+    //     if (isset($_SESSION['status']) && $_SESSION['status'] === true) {
+    //         header("Location:".$_SESSION['environnement']);
+    //     }
+    // }
 
     public function signOut():void
     {
